45f2f3905893b9ef118911b395664f6d702860ca
darkTang <2223191690@qq.com>
2023-08-10 10:29:30 +0800

    update mysql

--- user/models/core.go
+++ user/models/core.go
@@ -9,9 +9,13 @@ var DB *gorm.DB
 var err error
 
 func init() {
-	dsn := "root:ming199951@tcp(8.137.56.156:3306)/gin?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:ming199951@tcp(127.0.0.1:3306)/gin?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
+		return
+	}
+	// 这句代码会自动创建表结构，但是数据库必须手动创建
+	if err := DB.AutoMigrate(&User{}); err != nil {
 		return
 	}
 }
--- user/models/user.go
+++ user/models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct { // 默认表名为users
-	Id     string `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name   string `json:"name" form:"name"`
-	Number uint   `json:"number" form:"number"`
-	Mobile string `json:"mobile" form:"mobile"`
+	Id     string `json:"id" gorm:"type:bigint;primaryKey;autoIncrement"`
+	Name   string `json:"name" form:"name" gorm:"type:varchar(255);not null"`
+	Number uint   `json:"number" form:"number" gorm:"type:tinyint;unsigned;not null"`
+	Mobile string `json:"mobile" form:"mobile" gorm:"type:varchar(255);not null"`
 }
 
 func (User) TableName() string { // 可以修改默认的表名
