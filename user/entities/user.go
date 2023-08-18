30ca34efc2bac22ececc05f680b1dd9805a40b6f
darkTang <2223191690@qq.com>
2023-08-18 23:37:20 +0800

    feat: add table field

--- user/entities/user.go
+++ user/entities/user.go
@@ -5,7 +5,7 @@ type User struct { // 默认表名为users
 	Name     string `json:"name" gorm:"type:varchar(255);not null"`
 	Number   uint   `json:"number" gorm:"type:tinyint unsigned;not null"`
 	Mobile   string `json:"mobile" gorm:"type:varchar(255);not null"`
-	FromSide string `json:"fromSide" gorm:"type:char(1);default:'0'"`
+	FromSide string `json:"fromSide" gorm:"type:char(1);default:'1'"`
 }
 
 func (User) TableName() string { // 可以修改默认的表名
